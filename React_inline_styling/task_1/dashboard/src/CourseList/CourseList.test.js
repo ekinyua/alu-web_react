52ffb71c33bc9a5601224f9c9aab7fe9431702f5
Vulgence <e.kinyua@alustudent.com>
2024-09-20 00:14:12 +0300

    test debugging

--- React_inline_styling/task_1/dashboard/src/CourseList/CourseList.test.js
+++ React_inline_styling/task_1/dashboard/src/CourseList/CourseList.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import CourseList from "./CourseList";
-import CourseListRow from "./CourseListRow";
 import { shallow } from "enzyme";
 import { StyleSheetTestUtils } from "aphrodite";
 
